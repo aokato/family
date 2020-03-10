ae51a2fc22b59bfef20e872876333f843c2304ef
Ryota <onumaryotajh@gmail.com>
2020-03-10 10:57:17 +0900

    showを追加

---
+++ src/components/WebText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>{{ title }}</div>
+    <div v-html="Md"></div>
+  </div>
+</template>
+
+<script>
+import web11 from "@/assets/markdowns/web/1-1.md";
+import web12 from "@/assets/markdowns/web/1-2.md";
+export default {
+  props: {
+    folder: String,
+    title: String,
+  },
+  created() {
+    let which_md = this.folder + "/" + this.title;
+    this.whichtext(which_md);
+  },
+  data() {
+    return {
+      web11: web11,
+      web12: web12,
+    };
+  },
+  methods: {
+    whichtext: function(which_md) {
+      switch (which_md) {
+        case "web/1-1. 3ヶ月の流れ":
+          this.Md = this.web11;
+          break;
+
+        case "web/1-2. DemoDayについて":
+          this.Md = this.web12;
+          break;
+      }
+    },
+  },
+};
+</script>
--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,36 +1,27 @@
 <template>
   <div id="show">
-    {{ text.title }}
-    {{ text.content }}
+    <WebText :folder="folder" :title="title"></WebText>
   </div>
 </template>
 <script>
+import WebText from "@/components/WebText.vue";
 export default {
+  components: {
+    WebText,
+  },
   data() {
     return {
-      text: null,
+      folder: this.$route.params.course,
+      title: this.$route.params.title,
     };
-  },
-  created() {
-    this.axios
-      .get(
-        "https://family-rails-api.herokuapp.com/api/v1/web/" +
-          this.$route.params.id +
-          ".json"
-      )
-      .then(res => {
-        this.text = res.data;
-      });
   },
 };
 </script>
 <style lang="scss" scoped>
-@media screen and (min-width: 1024px) {
-  #show {
-  }
-}
-@media screen and (min-width: 700px) and (max-width: 1024px) {
-}
-@media screen and (min-width: 350px) and (max-width: 700px) {
+#show {
+  width: 100%;
+  margin-top: 60px;
+  min-height: 100vh;
+  background: rgba(64, 184, 131, 0.1);
 }
 </style>
